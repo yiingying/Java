8979ff1a7cbebbaaa457af024ee9803257a031b1
yiingying <54732769+yiingying@users.noreply.github.com>
2019-10-10 15:36:33 +0800

    20191010

---
+++ Test2_5/src/Test2_5.java
@@ -0,0 +1,7 @@
+public class Test2_5{
+    public static void main(String[] args) {
+        TestGrade tGrade=new TestGrade();
+        tGrade.print60(70);
+        tGrade.print60(50);
+    }
+}
---
+++ Test2_5/src/TestGrade.java
@@ -0,0 +1,8 @@
+public class TestGrade{
+    public void print60(int grade){
+        if(grade>60){
+            System.out.print("通过，成绩为：");
+        }
+        System.out.println(grade);
+    }
+}
